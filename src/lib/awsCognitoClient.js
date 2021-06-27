2785238d07d9f1df9b17f71b65e3da76241e8948
Camilo C. Pacheco <LutzMage@users.noreply.github.com>
2021-06-27 07:53:31 -0500

    [IAG-27] - Login system Terra - Beifong (#9)
    * IAG-27 Adds endpoint /users/login + tests + doc
    
    * IAG-27 Fixes problem in CI updating aws client

--- src/lib/awsCognitoClient.js
+++ src/lib/awsCognitoClient.js
@@ -3,25 +3,61 @@ require('cross-fetch/polyfill');
 const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
 
 class AwsCognitoClient {
-  signUp(email, password) {
+  userPool() {
+    const poolData = {
+      UserPoolId: process.env.AWS_POOL_ID,
+      ClientId: process.env.AWS_CLIENT_ID,
+    };
+
+    return new AmazonCognitoIdentity.CognitoUserPool(poolData);
+  }
+
+  signUp(username, password) {
     if (process.env.NODE_ENV === 'test')
       return new Promise((resolve) => resolve());
 
     return new Promise((resolve, reject) => {
-      const poolData = {
-        UserPoolId: process.env.AWS_POOL_ID,
-        ClientId: process.env.AWS_CLIENT_ID,
-      };
-
-      const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
-
-      userPool.signUp(email, password, [], null, (err, result) => {
+      this.userPool().signUp(username, password, [], null, (err, result) => {
         if (err) {
           reject(err);
           return;
         }
 
         resolve(result.user);
+      });
+    });
+  }
+
+  signIn(username, password) {
+    if (process.env.NODE_ENV === 'test') {
+      return new Promise((resolve) => {
+        const result = {
+          getIdToken: () => ({ getJwtToken: () => 'dummy_token' }),
+        };
+        resolve(result);
+      });
+    }
+
+    const authenticationData = {
+      Username: username,
+      Password: password,
+    };
+
+    const authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(
+      authenticationData
+    );
+
+    const userData = {
+      Username: username,
+      Pool: this.userPool(),
+    };
+
+    const cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+
+    return new Promise((resolve, reject) => {
+      cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: (result) => resolve(result),
+        onFailure: (err) => reject(err),
       });
     });
   }
